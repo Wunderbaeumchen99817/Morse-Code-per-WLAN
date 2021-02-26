57876684777a2dd039713beb99ffa15c587e3032
Wunderbaeumchen <33787544+Wunderbaeumchen@users.noreply.github.com>
2021-02-26 13:23:48 +0100

    Update script.py

--- script.py
+++ script.py
@@ -12,43 +12,49 @@ substrings = []
 delay_short = 1
 delay_long = 2
 ret = None
-url = "http://142.250.185.163:80"
+url = "http://192.168.2.110:80"
 def main():
     while True:
         text = input("Enter sentence (enter exit to leave): ").lower()
         #adding an exit-condition
         if (text=="exit"):
             break
-        for char in text:
-            #finding the correct letter
-            for item in letters:
-                if char in item:
-                    index = letters.index(item)
-                    print(letters[index])
-                    substrings = letters[index].split(":")
-                    morse = substrings[1].split()
-                    #print(morse)
+        try:
+            while True:
+                for char in text:
+                    #finding the correct letter
+                    for item in letters:
+                        if char in item:
+                            index = letters.index(item)
+                            print(letters[index])
+                            substrings = letters[index].split(":")
+                            morse = substrings[1].split()
+                            #print(morse)
 
-                    for element in morse:
-                        if (element=="0"):
-                            #on
-                            ret = requests.get(url+"/input?value=100")
-                            time.sleep(delay_short)
-                            #off
-                            ret = requests.get(url+"/input?value=0")
-                            print(ret.text)
-                        elif (element=="1"):
-                            test = 3
-                            #on
-                            ret = requests.get(url+"/input?value=100")
-                            time.sleep(delay_long)
-                            #off
-                            ret = requests.get(url+"/input?value=0")
-                            print(ret.text)
-                        print(element)
-            #adding "blank" for readability
-            time.sleep(delay_short)
-            print("\n")
+                            for element in morse:
+                                if (element=="0"):
+                                    #on
+                                    ret = requests.get(url+"/input?value=200")
+                                    time.sleep(delay_short)
+                                    #off
+                                    ret = requests.get(url+"/input?value=0")
+                                    #test = requests.get(url+"/init")
+                                    print(ret.text)
+                                elif (element=="1"):
+                                    test = 3
+                                    #on
+                                    ret = requests.get(url+"/input?value=200")
+                                    time.sleep(delay_long)
+                                    #off
+                                    ret = requests.get(url+"/input?value=0")
+                                    print(ret.text)
+                                print(element)
+                #adding "blank" for readability
+                time.sleep(delay_short)
+                print("\n")
+        except KeyboardInterrupt:
+                print("Interrupted")
+
         print("\n")
 
 main()
